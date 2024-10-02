12b7aa511ba14b1eb6176d882817b893c5298212
Xiaopu Zhang <k20125144@erc-hpc-comp001.create.kcl.ac.uk>
2024-10-02 16:47:47 +0100

    solve issue - no PCs remained

--- 03e-methylation_adjustment2.sh
+++ 03e-methylation_adjustment2.sh
@@ -45,82 +45,86 @@ else
 	echo "Running entire set on a single node using ${nthreads} threads."
 fi
 
-${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
-	${transformed_methylation_adjusted}.RData \
-	${nongenetic_meth_pcs_transformed}.txt \
-	${transformed_methylation_adjusted_pcs} \
-	transformed \
-	${nthreads} \
-	${meth_chunks} \
-	${i}
+if [ -f ${nongenetic_meth_pcs_transformed}.txt ]; then
+    ${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
+	    ${transformed_methylation_adjusted}.RData \
+	    ${nongenetic_meth_pcs_transformed}.txt \
+	    ${transformed_methylation_adjusted_pcs} \
+	    transformed \
+	    ${nthreads} \
+	    ${meth_chunks} \
+    	    ${i}
 
-if [ ${batch_number} == 1 ] && [ -f ${transformed_methylation_adjusted}.Female.chrX.RData ]; then
-${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
-	${transformed_methylation_adjusted}.Female.chrX.RData \
-	${nongenetic_meth_pcs_transformed}.txt \
-	${transformed_methylation_adjusted_pcs}.Female.chrX \
-	transformed \
-	${nthreads} \
-	${meth_chunks}
+    if [ ${batch_number} == 1 ] && [ -f ${transformed_methylation_adjusted}.Female.chrX.RData ]; then
+        ${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
+	    ${transformed_methylation_adjusted}.Female.chrX.RData \
+	    ${nongenetic_meth_pcs_transformed}.txt \
+	    ${transformed_methylation_adjusted_pcs}.Female.chrX \
+	    transformed \
+	    ${nthreads} \
+	    ${meth_chunks}
+    fi
+
+    if [ ${batch_number} == 1 ] && [ -f ${transformed_methylation_adjusted}.Male.chrX.RData ]; then
+    ${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
+	    ${transformed_methylation_adjusted}.Male.chrX.RData \
+	    ${nongenetic_meth_pcs_transformed}.txt \
+	    ${transformed_methylation_adjusted_pcs}.Male.chrX \
+	    transformed \
+	    ${nthreads} \
+	    ${meth_chunks}
+    fi
+
+    if [ ${batch_number} == 1 ] && [ -f ${transformed_methylation_adjusted}.Male.chrY.RData ]; then
+        ${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
+	    ${transformed_methylation_adjusted}.Male.chrY.RData \
+	    ${nongenetic_meth_pcs_transformed}.txt \
+	    ${transformed_methylation_adjusted_pcs}.Male.chrY \
+	    transformed \
+	    ${nthreads} \
+	    ${meth_chunks}
+    fi
 fi
 
-if [ ${batch_number} == 1 ] && [ -f ${transformed_methylation_adjusted}.Male.chrX.RData ]; then
-${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
-	${transformed_methylation_adjusted}.Male.chrX.RData \
-	${nongenetic_meth_pcs_transformed}.txt \
-	${transformed_methylation_adjusted_pcs}.Male.chrX \
-	transformed \
-	${nthreads} \
-	${meth_chunks}
-fi
+if [ -f ${nongenetic_meth_pcs_untransformed}.txt ]; then
+    ${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
+	    ${untransformed_methylation_adjusted}.RData \
+	    ${nongenetic_meth_pcs_untransformed}.txt \
+	    ${untransformed_methylation_adjusted_pcs} \
+	    untransformed \
+	    ${nthreads} \
+	    ${meth_chunks} \
+	    ${i}
 
-if [ ${batch_number} == 1 ] && [ -f ${transformed_methylation_adjusted}.Male.chrY.RData ]; then
-${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
-	${transformed_methylation_adjusted}.Male.chrY.RData \
-	${nongenetic_meth_pcs_transformed}.txt \
-	${transformed_methylation_adjusted_pcs}.Male.chrY \
-	transformed \
-	${nthreads} \
-	${meth_chunks}
-fi
+    if [ ${batch_number} == 1 ] && [ -f ${untransformed_methylation_adjusted}.Female.chrX.RData ]; then
+        ${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
+	    ${untransformed_methylation_adjusted}.Female.chrX.RData \
+	    ${nongenetic_meth_pcs_untransformed}.txt \
+	    ${untransformed_methylation_adjusted_pcs}.Female.chrX \
+	    untransformed \
+	    ${nthreads} \
+	    ${meth_chunks}
+    fi
 
-${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
-	${untransformed_methylation_adjusted}.RData \
-	${nongenetic_meth_pcs_untransformed}.txt \
-	${untransformed_methylation_adjusted_pcs} \
-	untransformed \
-	${nthreads} \
-	${meth_chunks} \
-	${i}
+    if [ ${batch_number} == 1 ] && [ -f ${untransformed_methylation_adjusted}.Male.chrX.RData ]; then
+        ${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
+	    ${untransformed_methylation_adjusted}.Male.chrX.RData \
+	    ${nongenetic_meth_pcs_untransformed}.txt \
+	    ${untransformed_methylation_adjusted_pcs}.Male.chrX \
+	    untransformed \
+	    ${nthreads} \
+	    ${meth_chunks}
+    fi
 
-if [ ${batch_number} == 1 ] && [ -f ${untransformed_methylation_adjusted}.Female.chrX.RData ]; then
-${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
-	${untransformed_methylation_adjusted}.Female.chrX.RData \
-	${nongenetic_meth_pcs_untransformed}.txt \
-	${untransformed_methylation_adjusted_pcs}.Female.chrX \
-	untransformed \
-	${nthreads} \
-	${meth_chunks}
-fi
-
-if [ ${batch_number} == 1 ] && [ -f ${untransformed_methylation_adjusted}.Male.chrX.RData ]; then
-${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
-	${untransformed_methylation_adjusted}.Male.chrX.RData \
-	${nongenetic_meth_pcs_untransformed}.txt \
-	${untransformed_methylation_adjusted_pcs}.Male.chrX \
-	untransformed \
-	${nthreads} \
-	${meth_chunks}
-fi
-
-if [ ${batch_number} == 1 ] && [ -f ${untransformed_methylation_adjusted}.Male.chrY.RData ]; then
-${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
-	${untransformed_methylation_adjusted}.Male.chrY.RData \
-	${nongenetic_meth_pcs_untransformed}.txt \
-	${untransformed_methylation_adjusted_pcs}.Male.chrY \
-	untransformed \
-	${nthreads} \
-	${meth_chunks}
+    if [ ${batch_number} == 1 ] && [ -f ${untransformed_methylation_adjusted}.Male.chrY.RData ]; then
+        ${R_directory}Rscript resources/methylation/adjust_nongeneticpcs.R \
+	    ${untransformed_methylation_adjusted}.Male.chrY.RData \
+	    ${nongenetic_meth_pcs_untransformed}.txt \
+	    ${untransformed_methylation_adjusted_pcs}.Male.chrY \
+	    untransformed \
+	    ${nthreads} \
+	    ${meth_chunks}
+    fi
 fi
 
 echo "Successfully adjusting non-genetic PCs"
