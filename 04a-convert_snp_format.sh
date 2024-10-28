728debc3e7b7ff1072c958a50adc583f0d7ca261
epzjlm <epzjlm@bristol.ac.uk>
2024-10-28 11:23:30 +0000

    chrX coding

--- 04a-convert_snp_format.sh
+++ 04a-convert_snp_format.sh
@@ -5,21 +5,27 @@ set -- $concatenated
 
 exec &> >(tee ${section_04a_logfile})
 print_version
-#Please read reasources/bin/hase/README_2.md
+#Please read resources/bin/hase/README_2.md
 #An expample is also provided below
 
 mkdir -p ${hase_dir_in}
 mkdir -p ${hase_converting}
 
-nX=grep ^X ${bfile}.bim | wc -l
+nX=`grep ^X ${bfile}.bim | wc -l`
 if [ "$nX" -gt "0" ]
 then
-${plink2} --bfile ${bfile} --make-bed --output-chr 26 --out ${hase_dir_in}
+
+perl -pe 's/^X\tX/23\t23/g' < ${bfile}.bim >${hase_dir_in}/data.bim
+cp ${bfile}.fam ${hase_dir_in}
+cp ${bfile}.bed ${hase_dir_in}
 else
 cp ${bfile}.bim ${hase_dir_in}
 cp ${bfile}.fam ${hase_dir_in}
 cp ${bfile}.bed ${hase_dir_in}
 fi
+
+nX=`grep ^X ${bfile}.bim | wc -l`
+
 
 python ${hase}/hase.py \
     -mode converting \
@@ -27,4 +33,7 @@ python ${hase}/hase.py \
     -o ${hase_converting} \
     -study_name ${study_name} # the name for your study 
 
+if [ "$nX" == "0" ]
+then
 echo "Successfully converted genetic data"
+fi
