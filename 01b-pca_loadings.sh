3371a2a3c3c12d5982b087e662ab6f4b2c9cd967
Haotian2020 <er20212@bristol.ac.uk>
2025-07-22 11:22:20 +0100

    add 01b to do pca loadings

---
+++ 01b-pca_loadings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source resources/setup.sh "$@"
+set -- $concatenated
+
+mkdir -p "${section_01_dir}/logs_b/"
+
+exec &> >(tee ${section_01b_logfile})
+print_version
+
+# please install hail from https://hail.is/docs/0.2/getting_started.html before running this script
+
+echo "Running global PCA"
+${Python3_directory}python "${scripts_directory}/resources/datacheck/global_pca.py" \
+    "${section_01_dir}/logs_b/hail.log" \
+    "${bfile_raw}" \
+    "${study_name}" \
+    "${scripts_directory}"
+
+# local pca plot to be done
+echo "Successfully completed godmc2 01b"
