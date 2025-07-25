9ef9710e284780758d112384a710cfbecca0786b
Haotian2020 <er20212@bristol.ac.uk>
2025-07-25 16:30:52 +0100

    mamba and conda check

--- 01b-pca_loadings.sh
+++ 01b-pca_loadings.sh
@@ -10,7 +10,25 @@ print_version
 
 # please install hail from https://hail.is/docs/0.2/getting_started.html before running this script
 
-mamba activate hail_env
+# Activate the environment
+if [ -z "$Python3_directory" ]; then
+    # Users are using system default R & Python, activate conda environment
+    if command -v mamba &> /dev/null; then
+        CONDA_CMD="mamba"
+    elif command -v conda &> /dev/null; then
+        CONDA_CMD="conda"
+    else
+        echo "Neither mamba nor conda found. Please install one of them first."
+        exit 1
+    fi
+    # Initialize shell for current session
+    eval "$($CONDA_CMD shell hook --shell bash)"
+    $CONDA_CMD activate hail_env
+    echo "Current conda environment: $CONDA_DEFAULT_ENV"
+else
+    # Users have specified custom R/Python directories
+    echo "Custom R/Python directories specified"
+fi
 
 echo "Running global PCA"
 ${Python3_directory}python "${scripts_directory}/resources/datacheck/global_pca.py" \
@@ -19,5 +37,4 @@ ${Python3_directory}python "${scripts_directory}/resources/datacheck/global_pca.
     "${study_name}" \
     "${scripts_directory}"
 
-# local pca plot to be done
 echo "Successfully completed godmc2 01b"
