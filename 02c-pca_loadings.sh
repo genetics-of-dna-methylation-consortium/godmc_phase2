17991fd270634384b2628419848f5969065b355d
Haotian2020 <er20212@bristol.ac.uk>
2025-08-06 22:22:12 +0100

    mamba first

--- 02c-pca_loadings.sh
+++ 02c-pca_loadings.sh
@@ -10,37 +10,36 @@ print_version
 
 # please install hail from https://hail.is/docs/0.2/getting_started.html before running this script
 
-# Activate the environment
 if [ -z "$Python3_directory" ]; then
-    if command -v conda &> /dev/null; then
-        if conda env list | grep -qE 'hail_env(\s|$)'; then
-            echo "found hail_env environment in conda"
-            echo "Using conda to run the script"
-            RUN_CMD="conda run -n hail_env"
-        elif command -v mamba &> /dev/null; then        
-            if mamba env list | grep -qE 'hail_env(\s|$)'; then
-                echo "found hail_env environment in mamba"
-                echo "Using mamba to run the script"
-                RUN_CMD="mamba run -n hail_env"
-            else
-                echo "ERROR: hail_env environment does not exist in conda or mamba. Please ensure hail_env exists."
-                exit 1
-            fi
-        else
-            echo "ERROR: hail_env environment does not exist in conda, and mamba not found."
-            exit 1
-        fi
-    elif command -v mamba &> /dev/null; then
+    if command -v mamba &> /dev/null; then
         if mamba env list | grep -qE 'hail_env(\s|$)'; then
             echo "found hail_env environment in mamba"
             echo "Using mamba to run the script"
             RUN_CMD="mamba run -n hail_env"
+        elif command -v conda &> /dev/null; then
+            if conda env list | grep -qE 'hail_env(\s|$)'; then
+                echo "found hail_env environment in conda"
+                echo "Using conda to run the script"
+                RUN_CMD="conda run -n hail_env"
+            else
+                echo "ERROR: hail_env environment does not exist in mamba or conda. Please ensure hail_env exists."
+                exit 1
+            fi
         else
-            echo "ERROR: hail_env environment does not exist in mamba. Please ensure hail_env exists."
+            echo "ERROR: hail_env environment does not exist in mamba, and conda not found."
+            exit 1
+        fi
+    elif command -v conda &> /dev/null; then
+        if conda env list | grep -qE 'hail_env(\s|$)'; then
+            echo "found hail_env environment in conda"
+            echo "Using conda to run the script"
+            RUN_CMD="conda run -n hail_env"
+        else
+            echo "ERROR: hail_env environment does not exist in conda. Please ensure hail_env exists."
             exit 1
         fi
     else
-        echo "ERROR: Neither conda nor mamba found. Please specify your Python3_directory in the config file."
+        echo "ERROR: Neither mamba nor conda found. Please specify your Python3_directory in the config file."
         exit 1
     fi
 else
