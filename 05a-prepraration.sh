65ab9812aab72d661c0fe35618259c1c3bc6b321
Xiaopu Zhang <k20125144@erc-hpc-login2.create.kcl.ac.uk>
2025-12-10 11:25:38 +0000

    fix male part

--- 05a-prepraration.sh
+++ 05a-prepraration.sh
@@ -59,7 +59,7 @@ then
     awk -F' ' '$34 == "M" {print $1}' ${covariates_combined}.txt > ${hase_in_male}/male_id
     awk 'NR==FNR {ids[$1]; next} $2 in ids' ${hase_in_male}/male_id ${hase_dir_in}/data.fam | cut -f 1-2 > ${hase_in_male}/male_fid_id
     
-    ${plink2} --bfile ${hase_dir_in}/data --keep ${hase_in_male}/male_fid_id --make-bed --out ${hase_in_male}/data
+    ${plink2} --bfile ${hase_dir_in}/data --keep ${hase_in_male}/male_fid_id --output-chr 26 --make-bed --out ${hase_in_male}/data
     nX=`grep ^X ${hase_in_male}/data.bim | wc -l`
     if [ "$nX" -gt "0" ]
     then
