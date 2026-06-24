75282566d5458a5afbeee1f01c0e095e5d40738b
Haotian2020 <er20212@bristol.ac.uk>
2026-06-24 13:04:40 +0100

    add light hase para

--- 04a-convert_snp_format.sh
+++ 04a-convert_snp_format.sh
@@ -8,19 +8,24 @@ print_version
 #Please read resources/bin/hase/README_2.md
 #An expample is also provided below
 
-mkdir -p ${hase_dir_in}
-mkdir -p ${hase_converting}
+mkdir -p ${light_hase_dir_in}
+mkdir -p ${light_hase_converting}
+mkdir -p ${light_hase}/data
+
+# assuming ref is in ori hase folder
+cp ${hase}/data/ref-hrc.ref.gz ${light_hase}/data/ref-hrc.ref.gz
+cp ${hase}/data/ref-hrc.ref_info.h5 ${light_hase}/data/ref-hrc.ref_info.h5
 
 echo "Flipping alleles into hrc reference allele order"
 
-hrc_ref_allele="${hase}/data/hrc_ref_allele.txt"
+hrc_ref_allele="${light_hase}/data/hrc_ref_allele.txt"
 
-zcat ${hase}/data/ref-hrc.ref.gz \
+zcat ${light_hase}/data/ref-hrc.ref.gz \
     | awk 'NR>1 {print $1 "\t" $3}' \
     > ${hrc_ref_allele}
 
 echo "ref-hrc.ref.gz lines including header:"
-zcat ${hase}/data/ref-hrc.ref.gz | wc -l
+zcat ${light_hase}/data/ref-hrc.ref.gz | wc -l
 
 echo "${hrc_ref_allele} lines without header:"
 wc -l ${hrc_ref_allele}
@@ -37,30 +42,31 @@ ${plink2} \
 	--threads "${nthreads}"
 
 nX=`grep ^X ${bfile}_haseinput.bim | wc -l`
+
 if [ "$nX" -gt "0" ]
 then
 
 #perl -pe 's/^X\tX/23\t23/g' < ${bfile}.bim >${hase_dir_in}/data.bim
 ${plink2} --bfile ${bfile}_haseinput --make-bed --output-chr 26 --out ${hase_dir_in}/data
-rm ${hase_dir_in}/data.log
+rm ${light_hase_dir_in}/data.log
 echo "This condition should not occur anymore; but if it does, the code above will fix the chrX coding in the bim file."
 else
-cp ${bfile}_haseinput.bim ${hase_dir_in}/data.bim
-cp ${bfile}_haseinput.fam ${hase_dir_in}/data.fam
-cp ${bfile}_haseinput.bed ${hase_dir_in}/data.bed
+cp ${bfile}_haseinput.bim ${light_hase_dir_in}/data.bim
+cp ${bfile}_haseinput.fam ${light_hase_dir_in}/data.fam
+cp ${bfile}_haseinput.bed ${light_hase_dir_in}/data.bed
 fi
 
-nX=`grep ^X ${hase_dir_in}/data.bim | wc -l`
+nX=`grep ^X ${light_hase_dir_in}/data.bim | wc -l`
 if [ "$nX" -gt "0" ]
 then
 echo "ERROR: wrong chrX coding"
 fi
 
 
-python ${hase}/hase.py \
+python ${light_hase}/light_hase.py \
     -mode converting \
-    -g ${hase_dir_in} \
-    -o ${hase_converting} \
+    -g ${light_hase_dir_in} \
+    -o ${light_hase_converting} \
     -study_name ${study_name} # the name for your study 
 
 echo "Successfully converted genetic data"
