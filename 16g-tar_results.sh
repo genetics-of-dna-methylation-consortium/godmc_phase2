5ae1e275f184fa203ee533bfeedf8bfcb0dec9d1
Haotian2020 <er20212@bristol.ac.uk>
2026-07-06 00:10:46 +0100

    excl hase feather files

--- 16g-tar_results.sh
+++ 16g-tar_results.sh
@@ -4,14 +4,19 @@ source resources/setup.sh "$@"
 set -- $concatenated
 
 mkdir -p "${section_16_dir}/logs_g"
-exec &> >(tee ${section_16g_logfile})
+exec &> >(tee "${section_16g_logfile}")
 print_version
 
-cd $home_directory
+cd "${home_directory}"
 
 suff="tgz"
 flags="czf"
+archive="${home_directory}/results/${study_name}_16.${suff}"
 
-tar ${flags} ${home_directory}/results/${study_name}_16.${suff} ${scripts_directory}/config ${scripts_directory}/resources/parameters ${home_directory}/results/16
+tar ${flags} "${archive}" \
+    --exclude="results/16/positive_control_validation/hase/*/run/*" \
+    "${scripts_directory}/config" \
+    "${scripts_directory}/resources/parameters" \
+    "results/16"
 
 echo "Successfully created results archives of module 16"
