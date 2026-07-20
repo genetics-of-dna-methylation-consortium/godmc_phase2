d3d1bdddf60b16146862e7fa99d0e14a28f0970a
Xiaopu Zhang <k20125144@erc-hpc-login1.create.kcl.ac.uk>
2026-07-20 16:05:13 +0100

    interaction scripts

---
+++ 06g.interaction_trans_candidateCpGs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source resources/setup.sh "$@"
+set -- $concatenated
+
+mkdir -p ${section_06_dir}/logs_g
+mkdir -p ${section_06_dir}/GEI_trans/candidate_CpGs
+
+exec &> >(tee ${section_06g_logfile})
+print_version
+
+source ~/miniforge3/etc/profile.d/mamba.sh
+mamba activate tensorqtl_godmc
+
+chr=$1
+
+echo "Start to run 06g chr ${chr} at $(date)"
+python ${scripts_directory}/resources/methylation/interaction_trans.py \
+    ${tabfile}.prunedSNPs.chr${chr} \
+    ${meth_vmeQTL_directory}/vmeQTL_phase2/adjustcovs_cpg_phase2_cpg_of_interest.bed.gz \
+    ${envs_input} \
+    ${section_06_dir}/GEI_trans/candidate_CpGs/snp_chr${chr} \
+    0 \
+    0.05
+
+echo "06g chr ${chr} has been done successfully at $(date)"
