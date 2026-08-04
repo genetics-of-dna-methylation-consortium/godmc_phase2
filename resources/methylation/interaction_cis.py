e48c4b6c1e8bce9fb633f9e4581294f2e791508e
Xiaopu Zhang <k20125144@erc-hpc-comp230.create.kcl.ac.uk>
2026-08-04 13:47:18 +0100

    save genotype encoding

--- resources/methylation/interaction_cis.py
+++ resources/methylation/interaction_cis.py
@@ -22,13 +22,15 @@ phenotype_df = phenotype_df[E_df.index]
 
 pr = genotypeio.PlinkReader(plink_prefix_path)
 genotype_df = pr.load_genotypes()
-variant_df = pr.bim.set_index('snp')[['chrom', 'pos']]
 print("Any -9 before imputation?", (genotype_df == -9).values.any())
 genotype_df = genotype_df.astype(np.float32)
 genotype_df[genotype_df == -9] = np.nan
 genotype_df = genotype_df.apply(lambda row: row.fillna(row.mean()), axis=1)
 print("Any -9 after imputation?", (genotype_df == -9).values.any())
 print("Any remaining NaNs after imputation?", genotype_df.isna().values.any())
+
+variant_df = pr.bim.set_index('snp')[['chrom', 'pos', 'a0', 'a1']]
+variant_df.to_csv(output_dir + '/Allele_info.csv', index=True)
 
 def runGE(Env):
     E_df_tmp = E_df[[Env]].dropna()
