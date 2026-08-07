ce34a540900c64c10a66e2b8bcddff4fb1136d45
Xiaopu Zhang <k20125144@erc-hpc-login2.create.kcl.ac.uk>
2026-08-07 13:19:20 +0100

    some samples do not have E

--- resources/methylation/interaction_cis.py
+++ resources/methylation/interaction_cis.py
@@ -33,7 +33,7 @@ print("Any remaining NaNs after imputation?", genotype_df.isna().values.any())
 variant_df = pr.bim.set_index('snp')[['chrom', 'pos', 'a0', 'a1']]
 variant_df.to_csv(output_dir + '/Allele_info.csv', index=True)
 
-def runGE(Env):
+def runGE(Env, genotype_df, phenotype_df, E_df):
     E_df_tmp = E_df[[Env]].dropna()
 
     if E_df_tmp[Env].std() == 0 or np.isclose(E_df_tmp[Env].std(), 0):
@@ -44,14 +44,28 @@ def runGE(Env):
         print(f"[SKIP] '{Env}': Too few valid samples remaining ({len(E_df_tmp)})")
         return
 
+    E_df_tmp.index = E_df_tmp.index.astype(str)
+    genotype_df.columns = genotype_df.columns.astype(str)
+    common_iids = E_df_tmp.index.intersection(genotype_df.columns)
+
+    E_df_tmp1 = E_df_tmp.loc[common_iids]
+    genotype_df1 = genotype_df[common_iids]
+    phenotype_df1 = phenotype_df[common_iids]
+
+    same_iids_and_order = (E_df_tmp1.index.equals(genotype_df1.columns) and genotype_df1.columns.equals(phenotype_df1.columns))
+
+    if same_iids_and_order==False:
+        print("Different sample sizes in Env, Genotype, and Phenotype dataset")
+        return
+
     prefix_out = "GEI_chunk"+str(chunk)+"_chr"+str(chrom)+"_E_"+Env+".candidate"
     mapping_df = pd.read_csv(list1, sep="\t", names=['SNP','CpG','Pair'], header=None)
     
     try:
-        cis_df = cis.map_nominal(mapping_df, genotype_df, variant_df, 
-                phenotype_df, phenotype_pos_df, prefix_out,
+        cis_df = cis.map_nominal(mapping_df, genotype_df1, variant_df, 
+                phenotype_df1, phenotype_pos_df, prefix_out,
                 covariates_df=None,
-                interaction_df=E_df_tmp, maf_threshold_interaction=0,
+                interaction_df=E_df_tmp1, maf_threshold_interaction=0,
                 run_eigenmt=False, output_dir=output_dir, write_top=False, write_stats=True)
         print(f"[SUCCESS] Completed GxE for: '{Env}'")    
     except Exception as err:
@@ -59,4 +73,4 @@ def runGE(Env):
 
 for column_name in E_df.columns:
     print(f"Starting execution for: {column_name}")
-    runGE(column_name)
+    runGE(column_name, genotype_df, phenotype_df, E_df)
