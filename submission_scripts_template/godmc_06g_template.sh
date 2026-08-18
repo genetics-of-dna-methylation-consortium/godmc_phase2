c6d3b04dd59ab14d13f5d598c9f73f7c7659c3fe
Xiaopu Zhang <k20125144@erc-hpc-comp206.create.kcl.ac.uk>
2026-08-18 08:49:11 +0100

    split snps to chunks to avoid OOM

--- submission_scripts_template/godmc_06g_template.sh
+++ submission_scripts_template/godmc_06g_template.sh
@@ -3,7 +3,10 @@
 cd ..
 source ./config
 
-for i in $(seq 1 22);
+for i in $(seq 1 9);
 do 
-    sbatch --mem 128G ${scripts_directory}/06g-interaction_trans_candidateCpGs.sh $i
+    for chunk in $(seq 1 ${prune_sub});
+    do
+        sbatch -p interruptible_cpu,cpu --mem 128G ${scripts_directory}/06g-interaction_trans_candidateCpGs.sh $i ${chunk}
+    done
 done
